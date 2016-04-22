5f0f412ac5027b4ff5a427fd80b2a559cf3966fe
NHKN <bomhoe123@gmail.com>
2016-04-22 14:07:19 +0700

    -m

--- config/define_database.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("__HOST","localhost");
-define("__USER","root");
-define("__PASS","123456");
-define("__DB_NAME","shop");
-?>
--- public/process/logout.php
+++
@@ -1,9 +0,0 @@
-﻿<?php include 'Currencontext.php';
-if(!isset($_SESSION))
-session_start();
-
-$retUrl=$_GET["retUrl"];
-logOut();
-header("location: $retUrl");
-exit();
-?>
